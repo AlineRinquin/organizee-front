466a14617480a7cded94b72184f97d0887859dc9
Sana EL HIRI <sanaze.elhiri@gmail.com>
2022-02-25 09:52:06 +0100

    add member avec role ok account récupère les membre en focntion de la team

--- src/app/services/membre.service.ts
+++ src/app/services/membre.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Membre } from '../models/membre';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,7 @@ export class MembreService {
   apiUrl: string;
   tokenKey: string;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private tokenService: TokenService, private router: Router) {
     this.apiUrl = environment.apiUrl;
     this.tokenKey = environment.tokenKey;
    }
@@ -20,13 +22,27 @@ export class MembreService {
     return this.http.get(`${this.apiUrl}/membres/all`);
   }
 
+  getMembresByTeamId(): Observable<any> | void{
+    const teamId = this.tokenService.getCurrentTeamId();
+    if (teamId){
+    return this.http.get(`${this.apiUrl}/membres/team/${teamId}`);
+    }else {
+      this.router.navigate(['accueil']);
+    }
+
+  }
+
   getMembreId(id: any): Observable<any> {
     return this.http.get(`${this.apiUrl}/membres/` + id);
   }
 
-  addMembre(membre: Membre): Observable<any> {
-    console.log(membre);
-    return this.http.post(`${this.apiUrl}/membres/sign-up`, membre);
+  addMembre(membre: Membre): Observable<any> | void{
+    const teamId = this.tokenService.getCurrentTeamId();
+    if (teamId){
+    return this.http.post(`${this.apiUrl}/membres/add/${teamId}`, membre);
+    }else {
+      this.router.navigate(['accueil']);
+    }
   }
 
   deleteMembre(membre: Membre): Observable<any> {
