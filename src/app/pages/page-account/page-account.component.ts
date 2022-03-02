ab58fd0c6a93a94127ff11bf4dff469a50433a7d
Sana EL HIRI <sanaze.elhiri@gmail.com>
2022-03-02 19:14:43 +0100

    avancées page account/ modif side bar/modif card-member

--- src/app/pages/page-account/page-account.component.ts
+++ src/app/pages/page-account/page-account.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MembreService } from 'src/app/services/membre.service';
+import { TeamService } from 'src/app/services/team.service';
+import { TokenService } from 'src/app/services/token.service';
+
 
 
 @Component({
@@ -9,16 +13,28 @@ import { MembreService } from 'src/app/services/membre.service';
 })
 export class PageAccountComponent implements OnInit {
   public listMembres: any[];
+  currentUser: any;
+  currentTeam: any;
 
-  constructor(private membreService: MembreService) {
+  constructor(private membreService: MembreService,
+    private teamService: TeamService,
+    private tokenService: TokenService,
+    private router: Router) {
     this.listMembres = [];
    }
 
   ngOnInit(): void {
+    /** Récuperer la team du membre connecté **/
+    this.teamService.getTeamById()?.subscribe((team) => {
+      this.currentTeam = team;
+    });
+    /** Récuperer le membre connecté **/
+    this.membreService.getMembreId(this.tokenService.getCurrentMembreId()).subscribe((user) => {
+      this.currentUser = user;
+    });
+    /** Récuperer la liste des membres de la team **/
     this.membreService.getMembresByTeamId()?.subscribe((membres: any[]) => {
-      console.log(membres);
       this.listMembres = membres;
-
-  });
+    });
   }
 }
